d3172e2dbbb1fbb267f53a6cfca93828b112110e
Carlos Robles <carlos.robles97@gmail.com>
2025-10-14 18:48:49 -0500

    shuffle is working

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -25,18 +25,31 @@ onMounted(async () => {
       answer: wrong_answer,
     })
   })
+  answers.value = shuffle(answers.value)
+  // console.log(question.value)
 })
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--){
+    const j = Math.floor(Math.random() * (i + 1));
+    //swap
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array
+}
+
 </script>
 
 <template>
   <div v-if="question" class="">
     <BaseTitle>{{ question.category }}</BaseTitle>
       {{ question.question }}
-      <div v-for="answer in answers" v-html="answer.answer" :key="answer.id" class="">
-      </div>
+      
+    <div v-for="answer in answers" v-html="answer.answer" :key="answer.id" class="">
+  </div>
   </div>
   
-
   <div v-else class="">
     Loading...
   </div>
