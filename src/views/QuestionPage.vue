4e1cedfd462b6070cb6ce4a664b99fc336e17c04
Carlos Robles <carlos.robles97@gmail.com>
2025-10-20 17:18:06 -0500

    Added scoring into questions

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -5,12 +5,17 @@ import { useRoute } from 'vue-router';
 import BaseTitle from '@/components/BaseTitle.vue';
 import DifficultyChip from '@/components/DifficultyChip.vue';
 import MainScore from '@/components/MainScore.vue';
+import useScore from '@/composables/useScore';
+import router from '@/router';
 
 
 const api = useAPI()
 const question = ref(null)
 const route = useRoute()
 const answers = ref([])
+const { changeScore } = useScore()
+const notification = ref('')
+
 
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id)
@@ -19,6 +24,7 @@ onMounted(async () => {
     id: answers.value.length,
     correct: true,
     answer: question.value.correct_answer,
+    points: question.value.difficulty === 'easy' ? 10 : question.value.difficulty === 'medium' ? 20 : 30,
   })
 
   question.value.incorrect_answers.map((wrong_answer) => {
@@ -26,6 +32,7 @@ onMounted(async () => {
       id: answers.value.length,
       correct: false,
       answer: wrong_answer,
+      points: -5,
     })
   })
   answers.value = shuffle(answers.value)
@@ -42,17 +49,36 @@ const shuffle = (array) => {
   return array
 }
 
+// handle answer choices and award points
+const handleAnswer = (points) => {
+  changeScore(points)
+  if (points > 0) {
+    notification.value = 'CORRECT'
+  }
+  else {
+    notification.value = 'INCORRECT'
+  }
+
+  setTimeout(() => {
+    router.push('/homework6/')
+  }, 1000)
+}
 </script>
 
 <template>
   <div v-if="question" class="flex h-full w-full flex-col items-center gap-8  p-10">
-    <BaseTitle>{{ question.category }} - <MainScore></MainScore></BaseTitle>
+    <BaseTitle><MainScore></MainScore> &nbsp;
+      <span class="font-bold" :class="notification === 'CORRECT' ? 'text-green-500' : 'text-red-500'"> 
+        {{ notification }}
+      </span>
+    </BaseTitle>
       <!-- {{ question.question }} -->
       <div v-html="question.question" class="text-center text-2xl font-bold"></div>
     <div class="grid w-full flex-grow grid-cols-2 gap-8"> 
       <div v-for="answer in answers" 
       v-html="answer.answer" 
       :key="answer.id" 
+      @click="handleAnswer(answer.points)"
       class="bg-blue-500 flex items-center justify-center text-4xl rounded-lg text-white py-10 px-2">
       </div>
 
